90eb7c0ec6497cd89ad7050b6bff314c8a364c00
brendan <bwheelz360@gmail.com>
2022-05-20 19:50:59 +1000

    trying to merge

--- examples/ApertureOptimisation/ApertureOptimisation_main.py
+++ examples/ApertureOptimisation/ApertureOptimisation_main.py
@@ -15,9 +15,9 @@ optimisation_params['LowerBounds'] = np.array([1, 1, 10])
 # generate a random starting point between our bounds (it doesn't have to be random, this is just for demonstration purposes)
 # random_start_point = np.random.default_rng().uniform(optimisation_params['LowerBounds'], optimisation_params['UpperBounds'])
 # optimisation_params['start_point'] = random_start_point
-optimisation_params['start_point'] = np.array([2.46, 1.62, 35])
+optimisation_params['start_point'] = np.array([1.14, 1.73, 39.9])
 # Remember true values are  [1.82, 2.5, 27]
-optimisation_params['Nitterations'] = 5
+optimisation_params['Nitterations'] = 40
 # optimisation_params['Suggestions'] # you can suggest points to test if you want - we won't here.
 ReadMeText = 'This is a public service announcement, this is only a test'
 
